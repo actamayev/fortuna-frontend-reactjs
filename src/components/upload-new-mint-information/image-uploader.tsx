c12eb19ed270fea59204fa4bd36405619aec845b
atamayev <75034340+atamayev@users.noreply.github.com>
2024-04-01 19:47:56 -0400

    Create upload image component

---
+++ src/components/upload-new-mint-information/image-uploader.tsx
@@ -0,0 +1,83 @@
+import _ from "lodash"
+import { useState, ChangeEvent, useRef } from "react"
+import Button from "../button"
+
+interface Props {
+	selectedImage: File | null
+	setSelectedImage: (files: File | null) => void
+}
+
+export default function ImageUploader(props: Props) {
+	const { selectedImage, setSelectedImage } = props
+	const [previewUrl, setPreviewUrl] = useState<null | string>(null)
+	const fileInputRef = useRef<HTMLInputElement>(null)
+
+	const handleImageChange = (e: ChangeEvent<HTMLInputElement>): void => {
+		const files = e.target.files
+
+		if (files && files.length > 0) {
+			const file = files[0]
+			setSelectedImage(file)
+
+			const newPreviewUrl = URL.createObjectURL(file)
+			setPreviewUrl(newPreviewUrl)
+		} else {
+			setSelectedImage(null)
+			setPreviewUrl(null)
+		}
+
+		if (fileInputRef.current) {
+			fileInputRef.current.value = ""
+		}
+	}
+
+	const removeImage = () => {
+		setSelectedImage(null)
+		setPreviewUrl(null)
+	}
+
+	const ShowImage = () => {
+		if (_.isNull(previewUrl)) return null
+		return (
+			<div className="preview-container">
+				<div style={{ position: "relative", display: "inline-block", margin: "5px" }}>
+					<img
+						src={previewUrl}
+						style={{ maxWidth: "35%", height: "auto" }}
+					/>
+					<Button
+						title="Remove"
+						colorClass="bg-red-600"
+						hoverClass="hover:bg-red-700"
+						onClick={removeImage}
+						className="text-white font-semibold"
+					/>
+				</div>
+			</div>
+		)
+	}
+
+	return (
+		<div className="mb-2">
+			<input
+				ref={fileInputRef}
+				type="file"
+				onChange={handleImageChange}
+				accept="image/*"
+				style={{ display: "none" }}
+				max={1}
+			/>
+			<Button
+				title="Choose an Image"
+				colorClass="bg-violet-500"
+				hoverClass="hover:bg-violet-600"
+				onClick={() => fileInputRef.current?.click()}
+				className="text-white font-semibold"
+				disabled={!_.isNull(selectedImage)}
+			/>
+
+			<ShowImage />
+		</div>
+	)
+}
+
--- src/pages/creator/upload-content.tsx
+++ src/pages/creator/upload-content.tsx
@@ -1,11 +1,14 @@
 import _ from "lodash"
+import { useState } from "react"
 import { observer } from "mobx-react"
 import CreatorHeader from "../../components/creator-header"
 import { useAuthContext } from "../../contexts/auth-context"
 import ShowAuthToNullUser from "../../components/show-auth-to-null-user"
+import ImageUploader from "../../components/upload-new-mint-information/image-uploader"
 
 function UploadContent() {
 	const authClass = useAuthContext()
+	const [selectedImage, setSelectedImage] = useState<File | null>(null)
 
 	if (_.isNull(authClass.accessToken)) {
 		return <ShowAuthToNullUser whereToNavigate="/creator/my-content" />
@@ -15,6 +18,10 @@ function UploadContent() {
 		<>
 			<CreatorHeader />
 			Upload content
+			<ImageUploader
+				selectedImage={selectedImage}
+				setSelectedImage={setSelectedImage}
+			/>
 		</>
 	)
 }
