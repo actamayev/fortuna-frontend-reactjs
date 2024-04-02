5ff81cb253ca5a98a3766cfa280a8d1a9c04bd55
atamayev <75034340+atamayev@users.noreply.github.com>
2024-04-02 00:09:07 -0400

    Trouble uploading file

--- src/services/upload-data-service.ts
+++ src/services/upload-data-service.ts
@@ -1,13 +1,17 @@
 import { AxiosResponse } from "axios"
 import FortunaHttpClient from "../classes/fortuna-http-client"
+import FormData from "form-data"
 
 export default class UploadDataService {
 	constructor(private readonly httpClient: FortunaHttpClient) {
 	}
 
-	async uploadImageToS3(image: File): Promise<AxiosResponse<UploadImageToS3 | MessageResponse | ErrorResponse>> {
+	async uploadImageToS3(file: File): Promise<AxiosResponse<UploadImageToS3 | MessageResponse | ErrorResponse>> {
+		const formData = new FormData()
+		formData.append("file", file)
+
 		return await this.httpClient.http.post<UploadImageToS3 | MessageResponse | ErrorResponse>(
-			"/devnet/upload/upload-image-to-s3", { image }
+			"/devnet/upload/upload-image-to-s3", formData
 		)
 	}
 }
