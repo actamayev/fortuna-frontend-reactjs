280a0c1b05cdf301c9854985335b041fa2d1952c
atamayev <75034340+atamayev@users.noreply.github.com>
2024-04-04 21:58:31 -0400

    Cleanup

--- src/components/my-ownership/single-content.tsx
+++ src/components/my-ownership/single-content.tsx
@@ -1,7 +1,5 @@
 import _ from "lodash"
 import { observer } from "mobx-react"
-import { Link } from "react-router-dom"
-import Button from "../button"
 import { useSolanaContext } from "../../contexts/solana-context"
 
 interface Props {
@@ -23,21 +21,7 @@ function SingleContent(props: Props) {
 				<h2 className="text-lg font-semibold mb-2">
 					{myContent.splName}
 					<img src={myContent.imageUrl} />
-
-					{/* Dr. {_.upperFirst(firstName || "")} {_.upperFirst(lastName || "")} */}
 				</h2>
-				{/* <Link
-					to={`/vet/${}`}
-					className="text-white inline-block"
-				>
-					<Button
-						colorClass="bg-orange-200"
-						hoverClass="hover:bg-orange-300"
-						className="font-bold py-2 px-4 rounded inline-block w-full"
-						title={`Click Me! NVI: ${nvi}`}
-						textColor="text-black"
-					/>
-				</Link> */}
 			</div>
 		</div>
 	)
