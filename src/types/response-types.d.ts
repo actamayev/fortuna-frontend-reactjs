ffb5bceece86b1ec4cd5263cf9a899171ccbb0b8
atamayev <75034340+atamayev@users.noreply.github.com>
2024-04-12 23:04:21 -0400

    Type cleanup

--- src/types/response-types.d.ts
+++ src/types/response-types.d.ts
@@ -9,36 +9,32 @@ declare global {
 	type AllCommonResponses = SuccessResponse | NonSuccessResponse
 
 	//Auth Responses:
-	type LoginSuccess = { accessToken: string }
-	type RegisterSuccess = { accessToken: string }
+	type LoginOrRegisterSuccess = { accessToken: string }
 
 	// Search Responses:
 	type SearchForUsersResponse = { usernames: { username: string }[] }
 	type BooleanResponse = { exists: boolean }
 
 	// Solana Responses:
-	type MintSPLResponse = { newSPLId: number, mintAddress: string }
+	type MintSPLResponse = {
+		newSPLId: number,
+		mintAddress: string
+	}
 	type RetrieveMyContentResponse = { creatorContentList: MyContent[] }
-
 	type TransferSolResponse = { solTransferData: SolanaTransaction }
-
 	type WalletBalanceResponse = {
 		balanceInSol: number,
 		balanceInUsd: number,
 		solPriceInUSD: number,
 		solPriceRetrievedTime: Date
 	}
-
-	type TransactionsResponse = {
-		transactions: SolanaTransaction[]
-	}
+	type TransactionsResponse = { transactions: SolanaTransaction[] }
 
 	// Upload Responses:
 	type UploadImageToS3 = {
 		imageUploadUrl: string
 		uploadedImageId: number
 	}
-
 	type UploadVideoToS3 = {
 		videoUploadUrl: string
 		uploadedVideoId: number
