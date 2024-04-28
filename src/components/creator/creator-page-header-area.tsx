dea99198d30ce9e32db6cadac100e7a3a7222669
atamayev <75034340+atamayev@users.noreply.github.com>
2024-04-28 19:18:22 -0400

    Simplify code

--- src/components/creator/creator-page-header-area.tsx
+++ src/components/creator/creator-page-header-area.tsx
@@ -1,25 +1,17 @@
-import _ from "lodash"
-import { observer } from "mobx-react"
-import { useParams } from "react-router-dom"
-import { useVideoContext } from "../../contexts/video-context"
+interface Props {
+	creatorData: CreatorDataHeldInClass
+}
 
-function CreatorPageHeaderArea() {
-	const { creatorUsername } = useParams<{ creatorUsername: string }>()
-	const videoClass = useVideoContext()
-
-	if (_.isUndefined(creatorUsername)) return null
-
-	const creatorData = videoClass.contextForCreatorData(creatorUsername)
-
-	if (_.isUndefined(creatorData)) return null
+export default function CreatorPageHeaderArea(props: Props) {
+	const { creatorData } = props
 
 	return (
-		<div className="flex items-center p-4 space-x-4"> {/* Flex container with space between items */}
+		<div className="flex items-center p-4 space-x-4">
 			{creatorData.creatorProfilePictureUrl && (
 				<img
 					src={creatorData.creatorProfilePictureUrl}
-					alt={`Profile of ${creatorData.creatorUsername}`} // Added alt for accessibility
-					className="w-24 h-24 rounded-full object-cover" // Ensured object-cover for better image fit
+					alt={`Profile of ${creatorData.creatorUsername}`}
+					className="w-24 h-24 rounded-full object-cover"
 				/>
 			)}
 			<div className="text-3xl dark:text-white">
@@ -28,5 +20,3 @@ function CreatorPageHeaderArea() {
 		</div>
 	)
 }
-
-export default observer(CreatorPageHeaderArea)
--- src/components/creator/creator-page-video-map.tsx
+++ src/components/creator/creator-page-video-map.tsx
@@ -1,18 +1,11 @@
-import _ from "lodash"
-import { observer } from "mobx-react"
-import { useParams } from "react-router-dom"
-import { useVideoContext } from "../../contexts/video-context"
 import SingleCreatorPageVideo from "./single-creator-page-video"
 
-function CreatorPageVideoMap() {
-	const { creatorUsername } = useParams<{ creatorUsername: string }>()
-	const videoClass = useVideoContext()
+interface Props {
+	creatorData: CreatorDataHeldInClass
+}
 
-	if (_.isUndefined(creatorUsername)) return null
-
-	const creatorData = videoClass.contextForCreatorData(creatorUsername)
-
-	if (_.isUndefined(creatorData)) return null
+export default function CreatorPageVideoMap(props: Props) {
+	const { creatorData } = props
 
 	return (
 		<>
@@ -22,5 +15,3 @@ function CreatorPageVideoMap() {
 		</>
 	)
 }
-
-export default observer(CreatorPageVideoMap)
