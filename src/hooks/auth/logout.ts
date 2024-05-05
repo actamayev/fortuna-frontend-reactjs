3427afb6d6a7efd0c6f2d20721a1877de5a9a58f
atamayev <75034340+atamayev@users.noreply.github.com>
2024-05-04 20:19:33 -0400

    Change from CRLF to LF

--- src/hooks/auth/logout.ts
+++ src/hooks/auth/logout.ts
@@ -2,6 +2,7 @@ import { useCallback } from "react"
 import useTypedNavigate from "../navigate/typed-navigate"
 import { useAuthContext } from "../../contexts/auth-context"
 import { useSolanaContext } from "../../contexts/solana-context"
+import { useYoutTubeContext } from "../../contexts/youtube-context"
 import { usePersonalInfoContext } from "../../contexts/personal-info-context"
 import { useApiClientContext } from "../../contexts/fortuna-api-client-context"
 
@@ -10,15 +11,17 @@ export default function useLogout(): () => void {
 	const personalInfoClass = usePersonalInfoContext()
 	const fortunaApiClient = useApiClientContext()
 	const solanaClass = useSolanaContext()
+	const youTubeClass = useYoutTubeContext()
 	const navigate = useTypedNavigate()
 
 	const logout = useCallback((): void => {
 		personalInfoClass?.logout()
 		solanaClass?.logout()
+		youTubeClass?.logout()
 		authClass.logout()
 		fortunaApiClient.logout()
 		navigate("/")
-	}, [authClass, fortunaApiClient, navigate, personalInfoClass, solanaClass])
+	}, [authClass, fortunaApiClient, navigate, personalInfoClass, solanaClass, youTubeClass])
 
 	return logout
 }
