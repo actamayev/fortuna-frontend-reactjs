8eadbcb8b94f0ba9b88af08702c6065ccb562018
Ariel Tamayev <actamayev@gmail.com>
2024-05-09 01:32:08 -0400

    Shares available progress bar (#45)
    * Add progress bar
    
    * Home video styling
    
    * Home page styling
    
    * Update shares-available-progress-bar.tsx
    
    * Progress bar
    
    ---------
    
    Co-authored-by: atamayev <75034340+atamayev@users.noreply.github.com>

--- src/components/home-page/home-page-video-description-area.tsx
+++ src/components/home-page/home-page-video-description-area.tsx
@@ -1,6 +1,7 @@
 import _ from "lodash"
 import PricePerShareArea from "./price-per-share-area"
 import useNavigateToVideo from "../../hooks/navigate/navigate-to-video"
+import SharesAvailableProgressBar from "./shares-available-progress-bar"
 import useNavigateToCreator from "../../hooks/navigate/navigate-to-creator"
 
 interface Props {
@@ -13,7 +14,7 @@ export default function HomePageVideoDescriptionArea(props: Props) {
 	const navigateToCreatorPage = useNavigateToCreator()
 
 	return (
-		<div className="flex items-center pt-1 dark:text-white rounded-lg ml-1">
+		<div className="flex items-center pt-1 dark:text-white rounded-lg mx-1">
 			{video.creatorProfilePictureUrl && (
 				<img
 					src={video.creatorProfilePictureUrl}
@@ -22,23 +23,31 @@ export default function HomePageVideoDescriptionArea(props: Props) {
 					onClick={() => navigateToCreatorPage(video.creatorUsername)}
 				/>
 			)}
-			<div className="flex flex-col flex-grow">
-				<div className="text-lg font-semibold cursor-pointer" onClick={() => navigateToVideoPage(video.uuid)}>
-					{_.truncate(video.splName, { length: 32, omission: "..."})}
+			<div className="flex flex-col">
+				<div
+					className="text-lg font-semibold cursor-pointer"
+					style={{ maxWidth: "fit-content" }}
+					onClick={() => navigateToVideoPage(video.uuid)}
+				>
+					{_.truncate(video.splName, { length: 32, omission: "..." })}
 				</div>
 				<div
 					className="text-sm text-gray-600 hover:text-black dark:text-gray-300 hover:dark:text-gray-100 cursor-pointer"
+					style={{ maxWidth: "fit-content" }}
 					onClick={() => navigateToCreatorPage(video.creatorUsername)}
 				>
 					{video.creatorUsername}
 				</div>
 			</div>
-			<div className="flex flex-col items-end mr-1">
+			<div className="ml-auto flex flex-col items-end space-y-3">
 				<div className="text-xs mt-1">
 					<PricePerShareArea video={video} />
 				</div>
-				<div className="text-xs">
-					<span className="font-bold">Shares Available:</span> {video.sharesRemainingForSale}
+				<div className="w-full">
+					<SharesAvailableProgressBar
+						sharesRemainingForSale={video.sharesRemainingForSale}
+						totalShares={video.totalNumberShares}
+					/>
 				</div>
 			</div>
 		</div>
