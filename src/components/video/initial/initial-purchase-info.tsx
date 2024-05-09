120230d76b3e567effaf68d395b8f29f4e789114
atamayev <75034340+atamayev@users.noreply.github.com>
2024-05-09 04:18:13 -0400

    Purchase shares card simplification

--- src/components/video/initial/initial-purchase-info.tsx
+++ src/components/video/initial/initial-purchase-info.tsx
@@ -1,42 +1,10 @@
-import _ from "lodash"
-import { observer } from "mobx-react"
-import { useParams } from "react-router-dom"
-import Button from "../../button"
 import ReviewPurchaseButton from "./review-purchase-button"
-import { useAuthContext } from "../../../contexts/auth-context"
-import { useVideoContext } from "../../../contexts/video-context"
-import useTypedNavigate from "../../../hooks/navigate/typed-navigate"
 import SelectNumberSharesToPurchase from "./select-number-shares-to-purchase"
 
-function InitialPurchaseInfo() {
-	const authClass = useAuthContext()
-	const { videoUUID } = useParams<{ videoUUID: string }>()
-	const videoClass = useVideoContext()
-	const navigate = useTypedNavigate()
-
-	if (_.isUndefined(videoUUID)) return null
-	const video = videoClass.findVideoFromUUID(videoUUID)
-	if (_.isUndefined(video)) return null
-
-	if (_.isEqual(video.sharesRemainingForSale, 0) || video.splListingStatus === "SOLDOUT") {
-		return <div className="dark:text-white">Sold out</div>
-	}
-
-	if (_.isNull(authClass.accessToken)) {
-		return (
-			<Button
-				onClick={() => navigate("/register")}
-				colorClass="bg-blue-200"
-				hoverClass="hover:bg-blue-300"
-				title="Please create an account to purchase shares"
-				className="font-semibold"
-			/>
-		)
-	}
-
+export default function InitialPurchaseInfo() {
 	return (
 		<>
-			<div className="text-center font-semibold dark:text-white">Purchase Shares</div>
+			<div className="text-center font-semibold">Purchase Shares</div>
 			<div>
 				<SelectNumberSharesToPurchase />
 			</div>
@@ -45,5 +13,3 @@ function InitialPurchaseInfo() {
 		</>
 	)
 }
-
-export default observer(InitialPurchaseInfo)
