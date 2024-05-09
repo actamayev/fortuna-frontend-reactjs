2df7571590b6adb667b85d31d2054e332ea6e2fc
mintfortuna <167586092+mintfortuna@users.noreply.github.com>
2024-05-09 01:32:08 -0400

    Shares available progress bar (#45)
    * Add progress bar
    
    * Home video styling
    
    * Home page styling
    
    * Update shares-available-progress-bar.tsx
    
    * Progress bar
    
    ---------
    
    Co-authored-by: atamayev <75034340+atamayev@users.noreply.github.com>

---
+++ src/components/home-page/shares-available-progress-bar.tsx
@@ -0,0 +1,38 @@
+/* eslint-disable max-len */
+import _ from "lodash"
+import { useState } from "react"
+
+interface Props {
+	sharesRemainingForSale: number
+	totalShares: number
+}
+
+export default function SharesAvailableProgressBar(props: Props) {
+	const { sharesRemainingForSale, totalShares } = props
+	const [showTooltip, setShowTooltip] = useState(false)
+
+	const percent = _.round((1 - (sharesRemainingForSale / totalShares)) * 100)
+
+	return (
+		<div
+			className="bg-gray-400 rounded-full h-1.5 dark:bg-white relative"
+			style={{ width: "110px" }} // Set a fixed width for the progress bar container
+			onMouseEnter={() => setShowTooltip(true)}
+			onMouseLeave={() => setShowTooltip(false)}
+		>
+			<div
+				className="bg-blue-600 dark:bg-blue-400 h-1.5 rounded-full transition-all duration-100"
+				style={{ width: `${percent}%` }}
+			/>
+			<div
+				className={`absolute left-1/2 transform -translate-x-1/2 -translate-y-[calc(100%+40px)] mt-2 px-2 py-1 bg-black \
+                text-white text-xs rounded shadow-lg transition-opacity duration-100 ${showTooltip ? "opacity-100 visible border dark:border-yellow-200" : "opacity-0 invisible"}`}
+				style={{ transition: "opacity 0.1s ease-in-out", minWidth: "130px" }}
+			>
+				<div>{`${_.round(percent)}% sold`}</div>
+				<div>{`${sharesRemainingForSale} shares available`}</div>
+			</div>
+		</div>
+	)
+
+}
