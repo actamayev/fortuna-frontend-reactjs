5af900cd6e0bedff41ec3cfe64f0ed23c03fc704
atamayev <75034340+atamayev@users.noreply.github.com>
2024-05-15 12:11:03 -0400

    Dark mode

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -8,10 +8,12 @@ export default function Layout (props: Props) {
 	const { children } = props
 
 	return (
-		<div className="flex flex-col min-h-screen">
-			<HeaderNav />
-			<div className="flex-1 w-full dark:bg-gray-950 bg-white overflow-y-auto px-10 py-8 mt-14">
-				{children}
+		<div className="min-h-screen dark:bg-gray-950">
+			<div className="flex flex-col">
+				<HeaderNav />
+				<div className="flex-1 w-full overflow-y-auto px-10 py-8 mt-14">
+					{children}
+				</div>
 			</div>
 		</div>
 	)
