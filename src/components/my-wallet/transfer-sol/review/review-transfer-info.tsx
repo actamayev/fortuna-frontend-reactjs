11362bc8d7f221d8a5c4ce39e6be25202e5fc847
atamayev <75034340+atamayev@users.noreply.github.com>
2024-05-17 18:32:24 -0400

    Review transfer section

--- src/components/my-wallet/transfer-sol/review/review-transfer-info.tsx
+++ src/components/my-wallet/transfer-sol/review/review-transfer-info.tsx
@@ -1,17 +1,9 @@
-import _ from "lodash"
-import { observer } from "mobx-react"
 import FeeSection from "./fee-section"
+import TransferAmountSection from "./transfer-amount-section"
 import ConfirmTransactionButton from "./confirm-transaction-button"
 import ReviewTransferBackButton from "./review-transfer-back-button"
-import { useSolanaContext } from "../../../../contexts/solana-context"
-import { usePersonalInfoContext } from "../../../../contexts/personal-info-context"
 
-function ReviewTransferInfo() {
-	const solanaClass = useSolanaContext()
-	const personalInfoClass = usePersonalInfoContext()
-
-	if (_.isNull(solanaClass) || _.isNull(personalInfoClass)) return null
-
+export default function ReviewTransferInfo() {
 	return (
 		<>
 			<div className="flex flex-row justify-between items-center font-semibold w-full">
@@ -20,23 +12,9 @@ function ReviewTransferInfo() {
 					Review Transfer
 				</div>
 			</div>
-			<div>
-				Sending {" "}
-				{personalInfoClass.defaultCurrency === "usd" && (
-					<> ${_.round(solanaClass.transferSolDetails.transferAmount, 2)} to </>
-				)}
-				{personalInfoClass.defaultCurrency === "sol" && (
-					<> {_.round(solanaClass.transferSolDetails.transferAmount, 4)} SOL to </>
-				)}
-				<span className="font-semibold">
-					{solanaClass.transferSolDetails.transferOption === "username" && solanaClass.transferSolDetails.username}
-					{solanaClass.transferSolDetails.transferOption === "publicKey" && solanaClass.transferSolDetails.publicKey}
-				</span>
-			</div>
+			<TransferAmountSection />
 			Fee: <FeeSection />
 			<ConfirmTransactionButton />
 		</>
 	)
 }
-
-export default observer(ReviewTransferInfo)
