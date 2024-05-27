6a0ec49fd0517e7e210b747560c7342f6e71ad3d
atamayev <75034340+atamayev@users.noreply.github.com>
2024-05-27 15:19:08 -0400

    Re-combine the retrieve wallet balance

--- src/hooks/solana/retrieve-wallet-balance-use-effect.ts
+++ src/hooks/solana/retrieve-wallet-balance-use-effect.ts
@@ -1,11 +1,35 @@
 import _ from "lodash"
-import { useEffect } from "react"
+import { useEffect, useCallback } from "react"
+import { Connection, LAMPORTS_PER_SOL, clusterApiUrl } from "@solana/web3.js"
 import { useSolanaContext } from "../../contexts/solana-context"
-import useRetrieveWalletBalance from "./retrieve-wallet-balance"
+import { usePersonalInfoContext } from "../../contexts/personal-info-context"
 
 export default function useRetrieveWalletBalanceUseEffect(): void {
 	const solanaClass = useSolanaContext()
-	const retrieveWalletBalance = useRetrieveWalletBalance()
+	const personalInfoClass = usePersonalInfoContext()
+
+	const retrieveWalletBalance = useCallback(async () => {
+		try {
+			if (
+				_.isNull(solanaClass) ||
+				_.isNull(solanaClass.walletPublicKey) ||
+				_.isNil(personalInfoClass?.username) ||
+				solanaClass.isRetrievingWalletDetails === true
+			) return
+
+			solanaClass.setIsRetrievingWalletDetails(true)
+			const connection = new Connection(clusterApiUrl("devnet"), "confirmed")
+
+			const balanceInLamports = await connection.getBalance(solanaClass.walletPublicKey)
+			const balanceInSol = balanceInLamports / LAMPORTS_PER_SOL
+
+			solanaClass.walletBalanceSol = balanceInSol
+		} catch (error) {
+			console.error(error)
+		} finally {
+			if (!_.isNull(solanaClass)) solanaClass.setIsRetrievingWalletDetails(false)
+		}
+	}, [solanaClass, personalInfoClass?.username])
 
 	useEffect(() => {
 		if (!_.isNil(solanaClass?.walletBalanceSol)) return
