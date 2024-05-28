fdfa8cb5366c87f71d6cc82631c65070ae812699
atamayev <75034340+atamayev@users.noreply.github.com>
2024-05-28 12:24:51 -0400

    Tooltip edits

--- src/components/tooltip.tsx
+++ src/components/tooltip.tsx
@@ -1,3 +1,6 @@
+import _ from "lodash"
+import React, { useState, useEffect } from "react"
+
 interface Props {
 	message: string
 	width?: string
@@ -6,17 +9,50 @@ interface Props {
 
 export default function Tooltip(props: Props) {
 	const { message, width, children } = props
+	const [isVisible, setIsVisible] = useState(false)
+	const [timeoutId, setTimeoutId] = useState<number | null>(null)
+
+	const showTooltip = () => {
+		if (!_.isNull(timeoutId)) {
+			clearTimeout(timeoutId)
+			setTimeoutId(null)
+		}
+		setIsVisible(true)
+	}
+
+	const hideTooltip = () => {
+		const id = setTimeout(() => {
+			setIsVisible(false)
+		}, 200)
+		setTimeoutId(id as unknown as number)
+	}
+
+	useEffect(() => {
+		return () => {
+			if (timeoutId) {
+				clearTimeout(timeoutId)
+			}
+		}
+	}, [timeoutId])
 
 	return (
-		<div className="ml-2 relative flex items-center group">
+		<div
+			className="ml-2 relative flex items-center group"
+			onMouseEnter={showTooltip}
+			onMouseLeave={hideTooltip}
+		>
 			<div className="cursor-pointer">{children}</div>
-			<div
-				className="absolute bottom-full right-0 mb-1 hidden group-hover:flex items-center justify-center \
-					p-1.5 bg-white text-black text-sm rounded-md shadow-lg whitespace-normal max-w-sm min-w-max border border-gray-300"
-				style={{ width: width || "auto", minWidth: "10rem" }}
-			>
-				{message}
-			</div>
+			{isVisible && (
+				<div
+					className="absolute bottom-full right-0 mb-1 flex items-center justify-center \
+          				p-1.5 bg-white text-black text-sm rounded-md shadow-lg whitespace-normal max-w-sm min-w-max border border-gray-300"
+					style={{ width: width || "auto", minWidth: "10rem" }}
+					onMouseEnter={showTooltip}
+					onMouseLeave={hideTooltip}
+				>
+					{message}
+				</div>
+			)}
 		</div>
 	)
 }
