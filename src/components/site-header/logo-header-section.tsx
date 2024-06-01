f76937d1c32dc0582bedf0a1c0106c8468c8c8c8
atamayev <75034340+atamayev@users.noreply.github.com>
2024-06-01 14:49:59 -0400

    Edit logo

--- src/components/site-header/logo-header-section.tsx
+++ src/components/site-header/logo-header-section.tsx
@@ -9,18 +9,18 @@ function LogoHeaderSection() {
 		<div className="inline-flex items-center flex-grow-0 flex-shrink-0 z-10">
 			<Link
 				to="/"
-				className="flex items-center text-zinc-900 dark:text-zinc-200 \
-					font-semibold text-3xl flex-shrink-0 hover:text-blue-500"
+				className="flex items-center font-semibold text-3xl flex-shrink-0 \
+					text-zinc-900 dark:text-zinc-200 hover:text-blue-500 dark:hover:text-blue-500"
 			>
 				{personalInfoClass && (
 					<img
 						src={personalInfoClass.defaultSiteTheme === "dark" ? "/fortuna-logo-white.svg" : "/fortuna-logo-black.svg"}
 						alt="Logo"
-						className="mt-2"
-						style={{ height: "60px" }}
+						className="ml-1"
+						style={{ height: "40px", verticalAlign: "middle" }}
 					/>
 				)}
-				Fortuna
+				<span className="ml-2">Fortuna</span>
 			</Link>
 		</div>
 	)
