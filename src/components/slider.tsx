d1c7ecc421389ea784efbd73b0e5d3f780cf5cae
atamayev <75034340+atamayev@users.noreply.github.com>
2024-06-02 12:49:52 -0400

    Update slider.tsx

--- src/components/slider.tsx
+++ src/components/slider.tsx
@@ -33,11 +33,12 @@ export default function Slider(props: Props) {
 					position: "relative",
 					display: "inline-block",
 					backgroundColor: isHovered ? "rgba(128, 128, 128, 0.2)" : "transparent",
-					borderRadius: "50%"
+					borderRadius: "50%",
+					cursor: "pointer",
 				}}
 			>
 				<label
-					className="relative inline-block"
+					className="relative inline-block cursor-pointer"
 					style={{
 						width: "30px",
 						height: "17px",
@@ -45,6 +46,7 @@ export default function Slider(props: Props) {
 						top: "50%",
 						left: "50%",
 						transform: "translate(-50%, -50%)",
+						pointerEvents: "none"
 					}}
 				>
 					<input
@@ -56,7 +58,7 @@ export default function Slider(props: Props) {
 						style={{ opacity: 0, width: 0, height: 0 }}
 					/>
 					<span
-						className="absolute inset-0 transition"
+						className="absolute inset-0 transition cursor-pointer"
 						style={{
 							backgroundColor: "#ccc",
 							borderRadius: "17px",
@@ -64,12 +66,13 @@ export default function Slider(props: Props) {
 							left: 0,
 							right: 0,
 							bottom: 0,
-							transition: ".05s"
+							transition: ".05s",
+							pointerEvents: "none"
 						}}
 					/>
 					<span
 						className={
-							`absolute rounded-full transition-transform flex items-center justify-center \
+							`absolute rounded-full transition-transform flex items-center justify-center cursor-pointer \
               					${colorChangeOnToggle && checkedCondition ? "bg-blue-600" : "bg-white"}`
 						}
 						style={{
@@ -80,6 +83,7 @@ export default function Slider(props: Props) {
 							transform: checkedCondition ? "translateX(13px)" : "translateX(0)",
 							transition: ".05s",
 							borderRadius: "50%",
+							pointerEvents: "auto"
 						}}
 					>
 						{checkedCondition && rightIcon}
