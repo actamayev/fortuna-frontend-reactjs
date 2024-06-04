f375af5bbcf5a760ce43ee2ab9e3d15c4fcba907
atamayev <75034340+atamayev@users.noreply.github.com>
2024-06-04 13:00:04 -0400

    Update missing.tsx

--- src/pages/missing.tsx
+++ src/pages/missing.tsx
@@ -1,17 +1,19 @@
 import { Link } from "react-router-dom"
+import { FaHome } from "react-icons/fa"
 
 export default function Missing() {
 	return (
 		<div className="text-center">
-			<p className="text-lg text-zinc-600 mb-8 dark:text-zinc-200">
+			<div className="text-lg text-zinc-600 mb-8 dark:text-zinc-200">
 				Page Not Found
-			</p>
-			<Link
-				to="/"
-				className="bg-blue-500 hover:bg-blue-700 text-zinc-50 dark:text-zinc-950 dark:bg-blue-400 dark:hover:bg-blue-500
-					font-semibold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-			>
-				Return home
+			</div>
+			<Link to="/" className="inline-block">
+				<div
+					className="bg-blue-500 hover:bg-blue-600 text-zinc-50 rounded focus:outline-none focus:shadow-outline \
+            		dark:text-zinc-950 dark:bg-blue-400 dark:hover:bg-blue-500 inline-flex items-center px-4 py-2"
+				>
+					Return home <FaHome className="ml-2" size={20}/>
+				</div>
 			</Link>
 		</div>
 	)
