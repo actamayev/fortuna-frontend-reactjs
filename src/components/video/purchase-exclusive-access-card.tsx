148328e47525b53bdbccf43501b44c6b441ab632
atamayev <75034340+atamayev@users.noreply.github.com>
2024-06-10 16:54:43 -0400

    Update purchase-exclusive-access-card.tsx

--- src/components/video/purchase-exclusive-access-card.tsx
+++ src/components/video/purchase-exclusive-access-card.tsx
@@ -6,23 +6,31 @@ import { useAuthContext } from "../../contexts/auth-context"
 import { useCreatorContext } from "../../contexts/creator-context"
 import useTypedNavigate from "../../hooks/navigate/typed-navigate"
 import PurchaseInstantAccessOptions from "./instant-access-exclusive-content/purchase-instant-access-options"
+import { useVideoContext } from "../../contexts/video-context"
 
 interface Props {
 	videoUUID: string
 }
 
-// TODO: This card should go behind the footer when scrolling down
 function PurchaseExclusiveAccessCard(props: Props) {
 	const { videoUUID } = props
+	const videoClass = useVideoContext()
 	const authClass = useAuthContext()
 	const navigate = useTypedNavigate()
 	const creatorClass = useCreatorContext()
+	const video = videoClass.findVideoFromUUID(videoUUID)
 
 	const wasVideoCreatedByUser = useMemo(() => {
 		if (_.isNull(creatorClass) || _.isUndefined(videoUUID)) return true
 		return creatorClass.checkIfUuidExistsInContentList(videoUUID)
 	// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [creatorClass, creatorClass?.myContent, videoUUID])
+
+	if (_.isNil(video?.numberOfExclusivePurchasesSoFar)) {
+		return (
+			<>Not exclusive</>
+		)
+	}
 
 	if (authClass.isLoggedIn === false) {
 		return (
