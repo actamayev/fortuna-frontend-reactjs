d4eb5ab1860ff5fa5c6a058456ed4ea052240fd1
atamayev <75034340+atamayev@users.noreply.github.com>
2024-06-11 13:25:50 -0400

    Update get-current-exclusive-access-tier.ts

--- src/utils/video-access-tiers/get-current-exclusive-access-tier.ts
+++ src/utils/video-access-tiers/get-current-exclusive-access-tier.ts
@@ -2,14 +2,14 @@ import _ from "lodash"
 
 export default function getCurrentExclusiveAccessTier(video: SingleVideoDataFromBackend): number | null {
 	try {
-		let { tierData } = video
-		if (_.isNull(video.numberOfExclusivePurchasesSoFar)) return null
+		const { tierData, numberOfExclusivePurchasesSoFar } = video
+		if (_.isNull(numberOfExclusivePurchasesSoFar)) return null
 
 		let cumulativePurchases = 0
 
-		tierData = _.sortBy(tierData, ["tierNumber"])
+		const sortedTierData = _.sortBy(tierData, ["tierNumber"])
 
-		for (const tier of tierData) {
+		for (const tier of sortedTierData) {
 			if (_.isNull(tier.purchasesInThisTier)) {
 				// If there is no limit for this tier, return its tier number
 				return tier.tierNumber
@@ -17,7 +17,7 @@ export default function getCurrentExclusiveAccessTier(video: SingleVideoDataFrom
 
 			cumulativePurchases += tier.purchasesInThisTier
 
-			if (video.numberOfExclusivePurchasesSoFar < cumulativePurchases) {
+			if (numberOfExclusivePurchasesSoFar < cumulativePurchases) {
 				// If the number of purchases so far is within this tier's range, return its tier number
 				return tier.tierNumber
 			}
