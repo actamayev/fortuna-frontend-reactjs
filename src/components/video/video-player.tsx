46f437cac6583bdd8abe94e1c403b79eae1223b0
Ariel Tamayev <actamayev@gmail.com>
2024-06-12 15:17:47 -0400

    Blur locked thumbnail (#76)
    Co-authored-by: atamayev <75034340+atamayev@users.noreply.github.com>

--- src/components/video/video-player.tsx
+++ src/components/video/video-player.tsx
@@ -1,26 +1,35 @@
 interface Props {
-	videoUrl: string | undefined
+	video: SingleVideoDataFromBackend
 }
 
-// FUTURE TODO: Make the aspect ratio of the video player the same as the home screen thumnail picture.
 export default function VideoPlayer(props: Props) {
-	const { videoUrl } = props
+	const { video } = props
 
 	return (
 		<div className="w-full">
-			{videoUrl ? (
+			{video.videoUrl ? (
 				<video
 					controls
 					autoPlay
 					className="w-full h-full rounded-lg"
 					controlsList="nodownload"
 				>
-					<source src={videoUrl} type="video/mp4" />
+					<source src={video.videoUrl} type="video/mp4" />
 					Your browser does not support the video tag.
 				</video>
 			) : (
 				<div className="w-full h-full rounded-lg bg-zinc-100 dark:bg-zinc-800" style={{ aspectRatio: "16/9" }}>
-					{/* Placeholder content if needed */}
+					<div className="relative w-full h-full rounded-lg overflow-hidden">
+						<img
+							src={video.imageUrl}
+							className="w-full h-full object-cover"
+							alt="Video Thumbnail"
+						/>
+						<div
+							className="absolute inset-0 bg-zinc-800 bg-opacity-50"
+							style={{ backdropFilter: "blur(8px)" }}
+						/>
+					</div>
 				</div>
 			)}
 		</div>
