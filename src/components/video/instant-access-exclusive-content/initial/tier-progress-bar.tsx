ec1c58159b3fde07ec6b327db76e05a3b8d52235
atamayev <75034340+atamayev@users.noreply.github.com>
2024-06-14 22:51:48 -0400

    Perfect progress bar

--- src/components/video/instant-access-exclusive-content/initial/tier-progress-bar.tsx
+++ src/components/video/instant-access-exclusive-content/initial/tier-progress-bar.tsx
@@ -1,18 +1,21 @@
 export default function TierProgressBar() {
-	const progress = 40
+	const progress = 100
 
 	return (
 		<div className="flex items-center space-x-2 relative">
 			<div
-				className="absolute -top-2.5 left-0 text-black font-bold rounded-full w-6 h-6 border \
-					border-black flex items-center justify-center text-md"
+				className="absolute -top-2.5 left-0 text-black font-bold rounded-full \
+					w-6 h-6 border border-black flex items-center justify-center text-md"
 				style={{ backgroundColor: "rgb(250, 255, 0)" }}
 			>
 				1
 			</div>
 			<div className="flex items-center w-full h-8 bg-gray-200 rounded-full border border-black overflow-hidden">
+				<div className="bg-gray-200 h-full flex items-center justify-center ml-3 mr-1">
+					<span className="text-black font-black">$4</span>
+				</div>
 				<div
-					className="bg-red-600 h-full flex items-center justify-end pr-2 rounded-full"
+					className="bg-red-600 h-full flex items-center justify-end pr-2 rounded-full border border-black"
 					style={{ width: `${progress}%` }}
 				>
 					<span className="text-white font-medium">{progress}%</span>
