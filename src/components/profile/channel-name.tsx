b9fc78bbaeaf3a594f5a3a7546f0416f9717e98f
atamayev <75034340+atamayev@users.noreply.github.com>
2024-07-03 16:30:50 -0400

    Update channel-name.tsx

--- src/components/profile/channel-name.tsx
+++ src/components/profile/channel-name.tsx
@@ -1,5 +1,5 @@
 import { observer } from "mobx-react"
-import { useState, useCallback } from "react"
+import { useState, useCallback, useEffect } from "react"
 import Button from "../buttons/button"
 import { useCreatorContext } from "../../contexts/creator-context"
 import { usePersonalInfoContext } from "../../contexts/personal-info-context"
@@ -10,8 +10,17 @@ function ChannelName() {
 	const personalInfoClass = usePersonalInfoContext()
 	const addOrEditChannelName = useAddOrEditChannelName()
 	const [isLoading, setIsLoading] = useState(false)
-	const [channelName, setChannelName] = useState(creatorClass?.channelName || personalInfoClass?.username || "")
+	const [channelName, setChannelName] = useState("")
 	const maxLength = 60
+
+	useEffect(() => {
+		if (creatorClass?.channelName) {
+			setChannelName(creatorClass.channelName)
+		} else if (personalInfoClass?.username) {
+			setChannelName(personalInfoClass.username)
+		}
+	}, [creatorClass?.channelName, personalInfoClass?.username])
+
 
 	const handleChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
 		if (event.target.value.length <= maxLength) {
