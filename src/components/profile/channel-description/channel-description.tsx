a258047a7915db0bd0d8c44f325e352305a2fe70
atamayev <75034340+atamayev@users.noreply.github.com>
2024-07-04 14:34:14 -0400

    Styling

--- src/components/profile/channel-description/channel-description.tsx
+++ src/components/profile/channel-description/channel-description.tsx
@@ -2,19 +2,17 @@ import { observer } from "mobx-react"
 import { FaPencilAlt } from "react-icons/fa"
 import { useState, useCallback, useRef, useEffect } from "react"
 import HoverOutlineComponent from "../../hover-outline-component"
+import { useCreatorContext } from "../../../contexts/creator-context"
 import useDefaultSiteTheme from "../../../hooks/memos/default-site-theme"
 import SaveChannelDescriptionButton from "./save-channel-description-button"
-import { useCreatorContext } from "../../../contexts/creator-context"
 
-// eslint-disable-next-line max-lines-per-function
 function ChannelDescription() {
 	const creatorClass = useCreatorContext()
 	const [channelDescription, setChannelDescription] = useState("")
-	const [inputWidth, setInputWidth] = useState("100px")
 	const [isEditing, setIsEditing] = useState(false)
 	const maxLength = 5000
-	const spanRef = useRef<HTMLSpanElement>(null)
 	const defaultSiteTheme = useDefaultSiteTheme()
+	const textAreaRef = useRef<HTMLTextAreaElement>(null)
 
 	useEffect(() => {
 		if (creatorClass?.channelDescription) {
@@ -22,24 +20,19 @@ function ChannelDescription() {
 		}
 	}, [creatorClass?.channelDescription])
 
-	const updateWidth = useCallback((text: string) => {
-		if (spanRef.current) {
-			spanRef.current.textContent = text || " "
-			setInputWidth(`${spanRef.current.offsetWidth + 20}px`)
-		}
-	}, [])
-
 	useEffect(() => {
-		updateWidth(channelDescription)
-	}, [channelDescription, updateWidth])
+		if (textAreaRef.current) {
+			textAreaRef.current.style.height = "auto"
+			textAreaRef.current.style.height = `${textAreaRef.current.scrollHeight}px`
+		}
+	}, [channelDescription])
 
 	const handleChange = useCallback((event: React.ChangeEvent<HTMLTextAreaElement>) => {
 		const value = event.target.value
 		if (value.length <= maxLength) {
 			setChannelDescription(value)
-			updateWidth(value)
 		}
-	}, [updateWidth])
+	}, [])
 
 	const toggleEditMode = useCallback(() => {
 		setIsEditing(!isEditing)
@@ -52,19 +45,10 @@ function ChannelDescription() {
 			</label>
 			<div className="flex items-center">
 				<div className="relative flex flex-col">
-					<span
-						ref={spanRef}
-						className="invisible absolute whitespace-pre"
-						style={{
-							padding: "0 2px",
-							whiteSpace: "pre"
-						}}
-					>
-						{channelDescription}
-					</span>
 					{isEditing ? (
 						<>
 							<textarea
+								ref={textAreaRef}
 								className={`mt-1 p-1.5 border rounded text-zinc-950 border-zinc-100 dark:border-zinc-700 
 									dark:text-zinc-200 bg-white dark:bg-zinc-800 outline-none 
 									${channelDescription.length === maxLength ? "border-red-500 dark:border-red-500" : ""}`}
@@ -73,14 +57,13 @@ function ChannelDescription() {
 								maxLength={maxLength}
 								style={{
 									minWidth: "100px",
-									width: inputWidth,
 									paddingRight: "10px",
 									boxSizing: "border-box",
-									resize: "none"
+									resize: "none",
+									overflow: "hidden",
 								}}
-								rows={2} // You can adjust the number of rows as needed
+								rows={2}
 							/>
-
 							<span className="text-xs text-zinc-600 dark:text-zinc-400 mt-1">
 								{channelDescription.length}/{maxLength}
 							</span>
@@ -101,7 +84,7 @@ function ChannelDescription() {
 						onClickAction={toggleEditMode}
 						classes="flex items-center justify-center"
 					>
-						<FaPencilAlt color={defaultSiteTheme === "dark" ? "white" : "black"} size={15}/>
+						<FaPencilAlt color={defaultSiteTheme === "dark" ? "white" : "black"} size={15} />
 					</HoverOutlineComponent>
 				)}
 			</div>
