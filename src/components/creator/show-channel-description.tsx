5cdfc24ca2080fac83b3d941a9b5a1dc76036ccf
atamayev <75034340+atamayev@users.noreply.github.com>
2024-07-07 15:46:13 -0400

    Update show-channel-description.tsx

--- src/components/creator/show-channel-description.tsx
+++ src/components/creator/show-channel-description.tsx
@@ -17,7 +17,7 @@ function ShowChannelDescription(props: Props) {
 	return (
 		<div
 			className="text-zinc-600 dark:text-zinc-300 text-sm font-semibold \
-		hover:bg-zinc-200 dark:hover:bg-zinc-700 rounded cursor-pointer px-1 py-2.5"
+			hover:bg-zinc-200 dark:hover:bg-zinc-700 rounded cursor-pointer px-1 py-2.5"
 			style={{
 				width: "80vw",
 				wordWrap: "break-word",
