ecd9a7ca9a6be1c93c35159d1964ccc312092dbc
atamayev <75034340+atamayev@users.noreply.github.com>
2024-07-08 19:37:07 -0400

    Convert social links to proper casing

--- src/hooks/creator/social-links/add-or-edit-social-link.ts
+++ src/hooks/creator/social-links/add-or-edit-social-link.ts
@@ -4,6 +4,7 @@ import { isErrorResponses } from "../../../utils/type-checks"
 import { useCreatorContext } from "../../../contexts/creator-context"
 import { useApiClientContext } from "../../../contexts/fortuna-api-client-context"
 import { useNotificationsContext } from "../../../contexts/notifications-context"
+import convertSocialLinkToProperCasing from "../../../utils/convert-social-link-to-proper-casing"
 
 export default function useAddOrEditSocialLink(): (
 	socialLink: string,
@@ -28,12 +29,12 @@ export default function useAddOrEditSocialLink(): (
 			if (!_.isEqual(response.status, 200) || isErrorResponses(response.data)) {
 				creatorClass.removeSocialPlatformLink(socialPlatform)
 			}
-			notificationsClass.setPositiveNotification(`Saved ${_.upperFirst(socialPlatform)} link`)
+			notificationsClass.setPositiveNotification(`Saved ${convertSocialLinkToProperCasing(socialPlatform)} link`)
 		} catch (error) {
 			console.error(error)
 			creatorClass.removeSocialPlatformLink(socialPlatform)
 			notificationsClass.setNegativeNotification(
-				`Unable to add ${_.upperFirst(socialPlatform)} link at this time. Please reload page and try again.`
+				`Unable to add ${convertSocialLinkToProperCasing(socialPlatform)} link at this time. Please reload page and try again.`
 			)
 		}
 	}, [creatorClass, fortunaApiClient.creatorDataService, notificationsClass])
--- src/hooks/creator/social-links/remove-social-link.ts
+++ src/hooks/creator/social-links/remove-social-link.ts
@@ -4,6 +4,7 @@ import { isErrorResponses } from "../../../utils/type-checks"
 import { useCreatorContext } from "../../../contexts/creator-context"
 import { useNotificationsContext } from "../../../contexts/notifications-context"
 import { useApiClientContext } from "../../../contexts/fortuna-api-client-context"
+import convertSocialLinkToProperCasing from "../../../utils/convert-social-link-to-proper-casing"
 
 export default function useRemoveSocialLink(): (
 	socialPlatform: SocialPlatformKey,
@@ -34,11 +35,11 @@ export default function useRemoveSocialLink(): (
 			}
 
 			creatorClass.removeSocialPlatformLink(socialPlatform)
-			notificationsClass.setPositiveNotification(`Removed ${_.upperFirst(socialPlatform)}`)
+			notificationsClass.setPositiveNotification(`Removed ${convertSocialLinkToProperCasing(socialPlatform)}`)
 		} catch (error) {
 			console.error(error)
 			notificationsClass.setNegativeNotification(
-				`Unable to remove ${_.upperFirst(socialPlatform)} link at this time. Please reload page and try again.`
+				`Unable to remove ${convertSocialLinkToProperCasing(socialPlatform)} link at this time. Please reload page and try again.`
 			)
 		}
 	}, [creatorClass, fortunaApiClient.creatorDataService, notificationsClass])
