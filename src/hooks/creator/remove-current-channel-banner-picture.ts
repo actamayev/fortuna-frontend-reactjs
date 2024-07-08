449c565356d4b1264a4ec3f34fe2eb1cab2b58ee
atamayev <75034340+atamayev@users.noreply.github.com>
2024-07-08 18:18:59 -0400

    Show notification when deleting/save failure

--- src/hooks/creator/remove-current-channel-banner-picture.ts
+++ src/hooks/creator/remove-current-channel-banner-picture.ts
@@ -2,6 +2,7 @@ import _ from "lodash"
 import { useCallback } from "react"
 import { isErrorResponse } from "../../utils/type-checks"
 import { useCreatorContext } from "../../contexts/creator-context"
+import { useNotificationsContext } from "../../contexts/notifications-context"
 import { useApiClientContext } from "../../contexts/fortuna-api-client-context"
 
 export default function useRemoveCurrentChannelBannerPicture(): (
@@ -9,25 +10,27 @@ export default function useRemoveCurrentChannelBannerPicture(): (
 ) => Promise<void> {
 	const creatorClass = useCreatorContext()
 	const fortunaApiClient = useApiClientContext()
+	const notificationsClass = useNotificationsContext()
 
 	const removeCurrentChannelBannerPicture = useCallback(async (
 		setIsDeletingCurrentPicture: React.Dispatch<React.SetStateAction<boolean>>
 	): Promise<void> => {
+		if (_.isNull(creatorClass)) return
+		const { channelBannerUrl } = creatorClass
 		try {
-			if (_.isNull(creatorClass)) return
-
+			creatorClass.setChannelBannerUrl(null)
+			setIsDeletingCurrentPicture(false)
 			const response = await fortunaApiClient.creatorDataService.removeCurrentChannelBannerPicture()
 
 			if (!_.isEqual(response.status, 200) || isErrorResponse(response.data)) {
 				return
 			}
-
-			creatorClass.setChannelBannerUrl(null)
-			setIsDeletingCurrentPicture(false)
 		} catch (error) {
 			console.error(error)
+			creatorClass.setChannelBannerUrl(channelBannerUrl) // if fails, reset the url to what it previously was
+			notificationsClass.setNotification("Removing Channel Banner failed. Please refresh and try again")
 		}
-	}, [creatorClass, fortunaApiClient.creatorDataService])
+	}, [creatorClass, fortunaApiClient.creatorDataService, notificationsClass])
 
 	return removeCurrentChannelBannerPicture
 }
--- src/hooks/creator/remove-current-profile-picture.ts
+++ src/hooks/creator/remove-current-profile-picture.ts
@@ -2,6 +2,7 @@ import _ from "lodash"
 import { useCallback } from "react"
 import { isErrorResponse } from "../../utils/type-checks"
 import { useCreatorContext } from "../../contexts/creator-context"
+import { useNotificationsContext } from "../../contexts/notifications-context"
 import { useApiClientContext } from "../../contexts/fortuna-api-client-context"
 
 export default function useRemoveCurrentProfilePicture(): (
@@ -9,25 +10,27 @@ export default function useRemoveCurrentProfilePicture(): (
 ) => Promise<void> {
 	const creatorClass = useCreatorContext()
 	const fortunaApiClient = useApiClientContext()
+	const notificationsClass = useNotificationsContext()
 
 	const removeCurrentProfilePicture = useCallback(async (
 		setIsDeletingCurrentPicture: React.Dispatch<React.SetStateAction<boolean>>
 	): Promise<void> => {
+		if (_.isNull(creatorClass)) return
+		const { profilePictureUrl } = creatorClass
 		try {
-			if (_.isNull(creatorClass)) return
-
+			creatorClass.setProfilePictureUrl(null)
+			setIsDeletingCurrentPicture(false)
 			const response = await fortunaApiClient.creatorDataService.removeCurrentProfilePicture()
 
 			if (!_.isEqual(response.status, 200) || isErrorResponse(response.data)) {
 				return
 			}
-
-			creatorClass.setProfilePictureUrl(null)
-			setIsDeletingCurrentPicture(false)
 		} catch (error) {
 			console.error(error)
+			creatorClass.setProfilePictureUrl(profilePictureUrl)  // if fails, reset the url to what it previously was
+			notificationsClass.setNotification("Removing Profile picture failed. Please refresh and try again")
 		}
-	}, [fortunaApiClient.creatorDataService, creatorClass])
+	}, [creatorClass, fortunaApiClient.creatorDataService, notificationsClass])
 
 	return removeCurrentProfilePicture
 }
