e32c51f9cab13f56dee8e6f1aec4414823bce869
atamayev <75034340+atamayev@users.noreply.github.com>
2024-07-10 20:23:42 -0400

    Update likes-dislikes-ratio-section.tsx

--- src/components/studio/my-content/likes-dislikes-ratio-section.tsx
+++ src/components/studio/my-content/likes-dislikes-ratio-section.tsx
@@ -7,23 +7,40 @@ interface Props {
 function LikesDislikesRatioSection(props: Props) {
 	const { content } = props
 
-	if (content.numberOfLikes === 0) {
+	let likePercentage: number | string = 0
+
+	if (content.numberOfLikes === 0 && content.numberOfDislikes === 0) {
 		return (
-			<div>
-				0 likes
-				Progress bar (half full)
+			<div className="flex flex-col items-start">
+				<div className="text-sm text-zinc-600 dark:text-zinc-300">
+					No likes/dislikes
+				</div>
 			</div>
 		)
 	}
 
-	let likePercentage
-	if (content.numberOfDislikes === 0) likePercentage = 100
-	else likePercentage = (content.numberOfLikes / content.numberOfDislikes)
+	if (content.numberOfLikes === 0) likePercentage = 0
+	else if (content.numberOfDislikes === 0) likePercentage = 100
+	else {
+		likePercentage = ((content.numberOfLikes / (content.numberOfLikes + content.numberOfDislikes)) * 100).toFixed(1)
+	}
 
 	return (
-		<div>
-			{likePercentage}%
-			{content.numberOfLikes} like{content.numberOfLikes === 1 ? "" : "s"}
+		<div className="flex flex-col items-start space-y-5">
+			<div className="text-sm text-zinc-600 dark:text-zinc-300">
+				{likePercentage}%
+			</div>
+			<div className="text-sm text-zinc-600 dark:text-zinc-300">
+				{content.numberOfLikes} like{content.numberOfLikes !== 1 && "s"}
+			</div>
+			{content.numberOfLikes > 0 || content.numberOfDislikes > 0 ? (
+				<div className="w-5/6 bg-gray-200 dark:bg-gray-700 rounded-full h-1.5">
+					<div
+						className="bg-blue-500 h-1.5 rounded-full"
+						style={{ width: `${likePercentage}%` }}
+					></div>
+				</div>
+			) : null}
 		</div>
 	)
 }
