cf4dbe79f07d2092c65764039926943a1a5ff366
atamayev <75034340+atamayev@users.noreply.github.com>
2024-07-12 12:55:29 -0400

    Make memo observer

--- src/components/wallet/transfer-funds/initial/review-transfer-button.tsx
+++ src/components/wallet/transfer-funds/initial/review-transfer-button.tsx
@@ -1,38 +1,18 @@
 import _ from "lodash"
+import { useCallback } from "react"
 import { observer } from "mobx-react"
-import { useCallback, useMemo } from "react"
 import Button from "../../../buttons/button"
 import { useSolanaContext } from "../../../../contexts/solana-context"
+import useIsReviewTransferButtonDisabled from "../../../../hooks/solana/is-review-transfer-button-disabled"
 
 function ReviewTransferButton() {
 	const solanaClass = useSolanaContext()
+	const isReviewTransferButtonDisabled = useIsReviewTransferButtonDisabled()
 
 	const updateTransferFundsDetails = useCallback(() => {
 		if (_.isNull(solanaClass)) return
 		solanaClass.updateTransferFundsDetails("transferStage", "review")
 	}, [solanaClass])
-
-	const isButtonDisabled = useMemo(() => {
-		if (_.isNull(solanaClass)) return true
-		if (_.isEqual(solanaClass.transferFundsDetails.transferAmount, 0)) return true
-		if (
-			solanaClass.transferFundsDetails.transferOption === "username" &&
-			solanaClass.transferFundsDetails.isUsernameSelected === false
-		) return true
-		if (_.isNull(solanaClass.walletPublicKey)) return true
-		if (
-			solanaClass.transferFundsDetails.transferOption === "publicKey" &&
-			(
-				solanaClass.transferFundsDetails.doesPublicKeyExist === false ||
-				solanaClass.transferFundsDetails.publicKey === solanaClass.walletPublicKey.toString())
-		) {
-			return true
-		}
-		return false
-	// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [solanaClass, solanaClass?.transferFundsDetails.transferAmount, solanaClass?.transferFundsDetails.transferOption,
-		solanaClass?.transferFundsDetails.doesPublicKeyExist, solanaClass?.transferFundsDetails.isUsernameSelected,
-		solanaClass?.walletPublicKey, solanaClass?.transferFundsDetails.publicKey])
 
 	return (
 		<Button
@@ -41,7 +21,7 @@ function ReviewTransferButton() {
 			colorClass="bg-blue-200 dark:bg-blue-400"
 			hoverClass="hover:bg-blue-300 hover:dark:bg-blue-500"
 			className="font-semibold text-zinc-950"
-			disabled={isButtonDisabled}
+			disabled={isReviewTransferButtonDisabled}
 		/>
 	)
 }
