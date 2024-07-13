802ea183a64ecb1c0a460ebb2c15be74d113cca5
Ariel Tamayev <actamayev@gmail.com>
2024-07-13 19:10:15 -0400

    Url updates (#110)
    Co-authored-by: atamayev <75034340+atamayev@users.noreply.github.com>

--- src/components/site-footer/footer-support-section.tsx
+++ src/components/site-footer/footer-support-section.tsx
@@ -7,11 +7,11 @@ export default function FooterSupportSection() {
 			<FooterHeaderText headerTitle="Support" />
 			<LinkToExternalSite
 				title="Help Center"
-				link="https://help.mintfortuna.com/frequently-asked-questions"
+				link="https://help.createfortuna.com/frequently-asked-questions"
 			/>
 			<LinkToExternalSite
 				title="Privacy Policy"
-				link="https://help.mintfortuna.com/legal/privacy-policy"
+				link="https://help.createfortuna.com/legal/privacy-policy"
 			/>
 		</div>
 	)
