f4d86933d19a52a4e8848020af7484b906408288
atamayev <75034340+atamayev@users.noreply.github.com>
2024-07-14 19:05:34 -0400

    Update search-results.tsx

--- src/pages/search-results.tsx
+++ src/pages/search-results.tsx
@@ -3,6 +3,7 @@ import { observer } from "mobx-react"
 import { useParams } from "react-router-dom"
 import useVideoSearch from "../hooks/search/video-search"
 import { useVideoContext } from "../contexts/video-context"
+import BasicHelmet from "../components/helmet/basic-helmet"
 import SingleSearchItem from "../components/search/single-search-item"
 
 function SearchResults() {
@@ -13,7 +14,14 @@ function SearchResults() {
 	if (_.isUndefined(searchTerm)) return null
 
 	if (videoClass.isCurrentlySearching === true) {
-		return <div className="dark:text-zinc-200">Searching...</div>
+		return (
+			<>
+				<BasicHelmet pageTitleData="Fortuna | Exclusive Videos" />
+				<div className="dark:text-zinc-200">
+					Searching...
+				</div>
+			</>
+		)
 	}
 	if (_.isUndefined(videoClass.contextForSearchMap(searchTerm))) {
 		videoClass.setSearchTerm(searchTerm)
@@ -24,15 +32,25 @@ function SearchResults() {
 	if (_.isUndefined(searchResults)) return null
 
 	if (_.isEmpty(searchResults)) {
-		return <div className="dark:text-zinc-200">No results</div>
+		return (
+			<>
+				<BasicHelmet pageTitleData={`${searchTerm} | Fortuna`} />
+				<div className="dark:text-zinc-200">
+					No results
+				</div>
+			</>
+		)
 	}
 
 	return (
-		<div className="flex flex-col items-center space-y-3">
-			{searchResults.map((searchResult, index) => (
-				<SingleSearchItem key={index} searchResult={searchResult} />
-			))}
-		</div>
+		<>
+			<BasicHelmet pageTitleData={`${searchTerm} | Fortuna`} />
+			<div className="flex flex-col items-center space-y-3">
+				{searchResults.map((searchResult, index) => (
+					<SingleSearchItem key={index} searchResult={searchResult} />
+				))}
+			</div>
+		</>
 	)
 }
 
