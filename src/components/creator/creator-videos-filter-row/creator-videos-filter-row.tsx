c9de413709d55e1f292269a3e44c6973433fdde1
atamayev <75034340+atamayev@users.noreply.github.com>
2024-07-15 20:12:45 -0400

    Update creator-videos-filter-row.tsx

--- src/components/creator/creator-videos-filter-row/creator-videos-filter-row.tsx
+++ src/components/creator/creator-videos-filter-row/creator-videos-filter-row.tsx
@@ -5,7 +5,7 @@ import SortByTimeFrameAndPopularity from "./sort-by-time-frame-and-popularity"
 export default function CreatorVideosFilterRow() {
 	return (
 		<div className="flex justify-center w-2/3 mb-3">
-			<div className="flex flex-row text-zinc-600 dark:text-zinc-200 text-sm w-1/2 max-w-4xl mx-auto">
+			<div className="flex flex-row text-zinc-600 dark:text-zinc-200 text-sm w-1/2 mx-auto">
 				<div className="border border-zinc-700 dark:border-zinc-300 p-1 mr-2 rounded-xl">
 					<div className="flex flex-row">
 						<SortByTimeFrameAndPopularity />
