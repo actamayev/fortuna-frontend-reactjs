063a153fd1639c0e99d6d7ff19163e284506cb41
atamayev <75034340+atamayev@users.noreply.github.com>
2024-07-15 22:50:54 -0400

    Creator page styles

--- src/components/creator/creator-videos-map/single-creator-page-video.tsx
+++ src/components/creator/creator-videos-map/single-creator-page-video.tsx
@@ -1,9 +1,8 @@
-import _ from "lodash"
 import { useCallback } from "react"
 import { observer } from "mobx-react"
-import BeneathThumbnailSection from "./beneath-thumbnail-section"
 import GeneralizedVideoThumbnail from "../../generalized-video-thumbnail"
 import useNavigateToVideoPage from "../../../hooks/navigate/navigate-to-video-page"
+import CreatorVideoRightInformationSection from "./creator-video-right-information-section"
 
 interface Props {
 	videoData: VideoDataLessVideoUrl
@@ -20,7 +19,7 @@ function SingleCreatorPageVideo(props: Props) {
 	return (
 		<div
 			className="w-2/3 rounded-lg cursor-pointer mb-3
-			bg-zinc-100 dark:bg-zinc-800 hover:bg-zinc-200 dark:hover:bg-zinc-700"
+		bg-zinc-100 dark:bg-zinc-800 hover:bg-zinc-200 dark:hover:bg-zinc-700"
 			onClick={navigateToVideoPageCallback}
 		>
 			<div className="flex w-full">
@@ -28,21 +27,11 @@ function SingleCreatorPageVideo(props: Props) {
 					<GeneralizedVideoThumbnail
 						thumbnailData={videoData}
 						imageStyles={{
-							borderTopRightRadius: 0, borderBottomLeftRadius: 0, borderBottomRightRadius: 0
+							borderTopRightRadius: 0, borderBottomRightRadius: 0
 						}}
 					/>
 				</div>
-				<div className="flex flex-col justify-start w-1/2 px-3">
-					<div className="text-2xl font-semibold truncate dark:text-zinc-200 overflow-hidden text-ellipsis whitespace-nowrap">
-						{videoData.videoName}
-					</div>
-					<div className="text-base text-zinc-600 dark:text-zinc-300 font-light ">
-						{_.truncate(videoData.description, { length: 430 })}
-					</div>
-				</div>
-			</div>
-			<div className="w-1/2">
-				<BeneathThumbnailSection videoData={videoData} />
+				<CreatorVideoRightInformationSection videoData={videoData} />
 			</div>
 		</div>
 	)
