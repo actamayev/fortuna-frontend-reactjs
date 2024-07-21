efc5a39a2064d2061cf46b0a4448440991cd0aff
atamayev <75034340+atamayev@users.noreply.github.com>
2024-07-20 20:15:11 -0400

    Update channel-description.tsx

--- src/components/studio/studio-header/channel-description/channel-description.tsx
+++ src/components/studio/studio-header/channel-description/channel-description.tsx
@@ -3,8 +3,8 @@ import { observer } from "mobx-react"
 import { useState, useCallback, useRef, useEffect } from "react"
 import { useCreatorContext } from "../../../../contexts/creator-context"
 import ChannelDescriptionTextInput from "./channel-description-text-input"
-import useAddOrEditChannelDescription from "../../../../hooks/creator/add-or-edit-channel-description"
 import useEscapeListenerUseEffect from "../../../../hooks/listeners/escape-key-listener-use-effect"
+import useAddOrEditChannelDescription from "../../../../hooks/creator/add-or-edit-channel-description"
 
 function ChannelDescription() {
 	const creatorClass = useCreatorContext()
