8db44140e2507fb6ca7c87647e66409a25115459
atamayev <75034340+atamayev@users.noreply.github.com>
2024-07-25 14:37:35 -0400

    Update show-number-videos.tsx

--- src/components/studio/studio-header/show-number-videos.tsx
+++ src/components/studio/studio-header/show-number-videos.tsx
@@ -14,8 +14,8 @@ function ShowNumberVideos() {
 	}, [creatorClass?.numberOfUnlistedVideos])
 
 	return (
-		<div className="text-zinc-600 dark:text-zinc-300 text-sm ml-1">
-			• {numberVideos} video{numberVideos === 1 ? "" : "s"}
+		<div className="text-zinc-600 dark:text-zinc-300 text-sm">
+			&nbsp;• {numberVideos} video{numberVideos === 1 ? "" : "s"}
 			{numberOfUnlistedVideos > 0 && (
 				<>
 					&nbsp;({numberVideos - numberOfUnlistedVideos} listed, {numberOfUnlistedVideos} unlisted)
