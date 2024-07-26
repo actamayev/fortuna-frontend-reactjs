c9992ec00a34381e25915e795d9185f1692d2252
atamayev <75034340+atamayev@users.noreply.github.com>
2024-07-26 18:29:32 -0400

    Moving logic

--- src/components/site-header/login-logout-header-item.tsx
+++ src/components/site-header/login-logout-header-item.tsx
@@ -1,20 +1,12 @@
-import _ from "lodash"
-import { useMemo } from "react"
 import { observer } from "mobx-react"
 import { useLocation } from "react-router-dom"
 import NullUserNavLink from "../null-user-nav-link"
 import { useAuthContext } from "../../contexts/auth-context"
 import HeaderDropdown from "./profile-dropdown/header-dropdown"
-import { usePersonalInfoContext } from "../../contexts/personal-info-context"
 
 function LoginLogoutHeaderItem() {
 	const authClass = useAuthContext()
 	const location = useLocation()
-	const personalInfoClass = usePersonalInfoContext()
-
-	const username = useMemo(() => {
-		return personalInfoClass.username
-	}, [personalInfoClass.username])
 
 	if (authClass.isLoggedIn === false) {
 		if (location.pathname === "/register" || location.pathname === "/login") {
@@ -22,8 +14,6 @@ function LoginLogoutHeaderItem() {
 		}
 		return <NullUserNavLink />
 	}
-
-	if (_.isNull(username)) return null
 
 	return <HeaderDropdown />
 }
