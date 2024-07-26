3770175fa2f55403b201e6c97542cf6ef1abca82
atamayev <75034340+atamayev@users.noreply.github.com>
2024-07-26 14:34:06 -0400

    Update video-data-right-of-search-thumbnail.tsx

--- src/components/search/video-data-right-of-search-thumbnail.tsx
+++ src/components/search/video-data-right-of-search-thumbnail.tsx
@@ -27,8 +27,8 @@ function VideoDataRightOfSearchThumbnail(props: Props) {
 	return (
 		<div className="flex flex-col justify-start w-7/12 px-3 my-1.5">
 			<div className="flex justify-between items-center">
-				<div className="flex-1 min-w-0 text-2xl font-semibold dark:text-zinc-200 overflow-hidden">
-					{_.truncate(videoName, { length: 60 })}
+				<div className="flex-1 min-w-0 text-2xl font-semibold dark:text-zinc-200 overflow-hidden text-ellipsis whitespace-nowrap">
+					{videoName}
 				</div>
 				<div className="text-zinc-500 dark:text-zinc-400 text-xs flex-shrink-0">
 					<div className="flex flex-row items-center">
